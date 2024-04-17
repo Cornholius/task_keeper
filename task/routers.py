2eff86762fccb78732391dd955f11274c1340817
lyi <y.layshkin@gmail.com>
2024-04-17 18:05:50 +0300

    мелкие правки

--- task/routers.py
+++ task/routers.py
@@ -1,10 +1,10 @@
 import json
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from database.db import get_async_session
 from task.models import Task
-from task.schemas import Task as TaskSchema
+from task.schemas import getTask, addTask
 
 
 router = APIRouter()
@@ -14,7 +14,7 @@ router = APIRouter()
 async def get_current_task(pk: int, session: AsyncSession = Depends(get_async_session)):
     query = select(Task).where(Task.id == pk)
     res = await session.execute(query)
-    result = TaskSchema.model_validate(*res.one_or_none(), from_attributes=True)
+    result = getTask.model_validate(*res.one_or_none(), from_attributes=True)
     return result
 
 
@@ -22,30 +22,25 @@ async def get_current_task(pk: int, session: AsyncSession = Depends(get_async_se
 async def get_all_tasks(session: AsyncSession = Depends(get_async_session)):
     query = select(Task)
     res = await session.execute(query)
-    result = [TaskSchema.model_validate(row[0], from_attributes=True) for row in res.all()]
+    result = [getTask.model_validate(row[0], from_attributes=True) for row in res.all()]
     return result
 
 
 @router.post('/add')
-async def new_task(request, session: AsyncSession = Depends(get_async_session)):
-    data = json.loads(request)
-    session.add(Task(**data))
+async def new_task(request: addTask, session: AsyncSession = Depends(get_async_session)):
+    qwe = [*request]
+    print('!!!!!/add', qwe)
+    # data = json.loads(request)
+    session.add(Task(*request))
     await session.commit()
 
 
 @router.patch('/patch/')
-async def new_task(request: Request, session: AsyncSession = Depends(get_async_session)):
-    print('!!!!!!!!!!!!!!!', request, type(request))
-    data = json.loads(request)
-    req = TaskSchema.model_validate(data)
-    qwe = await session.execute(select(Task).where(Task.id == req.id))
-    old_data = qwe.one()
-    for key, value in data.items():
-        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        print(old_data, type(old_data))
+async def new_task(task: getTask, session: AsyncSession = Depends(get_async_session)):
+    old_data = await session.get(Task, task.id)
+    for key, value in task:
         setattr(old_data, key, value)
     await session.commit()
-    return old_data
 
 
 @router.delete('/del/')
--- task/schemas.py
+++ task/schemas.py
@@ -2,8 +2,36 @@ from typing import Optional
 from pydantic import EmailStr, BaseModel
 
 
-class Task(BaseModel):
+class getTask(BaseModel):
     id: int
     title: Optional[str]
     data: str
     owner: str
+    model_config = {
+            "json_schema_extra": {
+                "examples": [
+                    {
+                        "id": 1,
+                        "title": "Task title",
+                        "data": "some data",
+                        "owner": "who owner",
+                    }
+                ]
+            }
+        }
+
+class addTask(BaseModel):
+    title: Optional[str]
+    data: str
+    owner: str
+    # model_config = {
+    #         "json_schema_extra": {
+    #             "examples": [
+    #                 {
+    #                     "title": "Task title",
+    #                     "data": "some data",
+    #                     "owner": "who owner",
+    #                 }
+    #             ]
+    #         }
+    #     }
